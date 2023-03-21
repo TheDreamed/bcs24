cadad855eb7a4d988e34dcb34b06fbc9195d1820
TheDreamed <markspencerjordan@gmail.com>
2023-03-21 17:45:23 +0800

    230321 - Main Activity
    230321 - Main Activity

--- m03/activity/activity_jordan.js
+++ m03/activity/activity_jordan.js
@@ -3,6 +3,9 @@ console.log("Hello, Welcome to De La Salle University!")
 function computeSupply(age, amountPerDay){
     total =  Math.round((365*age)*amountPerDay);
     console.log(`You need to ${total} to last you until the ripe old age of ${age}`)
+    console.log('Age:' + age)
+    console.log('Amount Per Day:'+ amountPerDay)
+    console.log('Rest of life total:' + total)
 }
 computeSupply(21, 2.5)
 computeSupply(31, 5)
